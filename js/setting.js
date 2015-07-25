4bfde85f560202abcd0cf4d220c1c4ec48d868f0
ugwis <ugwis@icloud.com>
2015-07-25 23:01:53 +0900

    アカウントの認証をカキカキ

--- js/setting.js
+++ js/setting.js
@@ -1,4 +1,6 @@
 
+var auth_window;
+
 function add_account(){
 	var oa = new OAuth(
 		"https://api.twitter.com/oauth/request_token",
@@ -6,20 +8,75 @@ function add_account(){
 		obj.option.consumer_key,
 		obj.option.consumer_secret,
 		"1.0",
-		"C:\\Users\\orn\Desktop\\node-webkit\\waftter\\index.html",
+		"http://auth.waftter.jp/auth/entry.cgi",
 		"HMAC-SHA1"
 	);
 	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
 		if (error) {
+			new Notification(err);
+			return;
 		} else {
-			childwin.push(gui.Window.open('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token,{
+			if(auth_window) auth_window.close();
+			auth_window = gui.Window.open('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token,{
 				'transparent': false,
 				"icon":"images/waftter_icon.png",
 				'toolbar': false,
 				'frame': true,
 				'width': 500,
 				'height': 400
-			}));
+			});
+			auth_window.on('closed',function(){
+				$.getJSON("http://auth.waftter.jp/auth/auth.cgi?oauth_token=" + oauth_token,{},function(data){
+					oa.getOAuthAccessToken(
+						data.oauth_token,
+						oauth_token_secret,
+						data.oauth_verifier,
+						function(error,oauth_access_token,oauth_access_token_secret,result){
+							if(err){
+								new Notification(err);
+								return;
+							}
+							var twitter = new twitter({
+								consumer_key: obj.option.consumer_key,
+								consumer_secret: obj.option.consumer_secret,
+								access_token_key: oauth_access_token,
+								access_token_secret: oauth_access_token_secret
+							});
+							twitter.verifyCredentials(function(err,data){
+								if(err){
+									new Notification(err);
+									return;
+								}
+								console.log(data);
+								/*obj.account.push({
+									"token":{
+										"consumer_key": obj.option.consumer_key,
+										"consumer_secret":obj.option.consumer_secret,
+										"access_token_key":oauth_access_token,
+										"access_token_secret":oauth_access_token_secret
+									},
+									"screen_name":""
+								})*/
+							});
+
+							message_send(1,{
+								type: "callback_status",
+								callback_status:{
+									type:"auth",
+									error: error,
+									access_token: oauth_access_token,
+									access_token_secret: oauth_access_token_secret
+								}
+							},session_no);
+							if(!error){
+								listen[session_no].access_token_key = oauth_access_token;
+								listen[session_no].access_token_secret = oauth_access_token_secret;
+							}
+						}
+					);
+				});
+			});
+			console.log(oauth_token_secret);
 //			authenticating_user_oauth_secret[oauth_token] = oauth_token_secret;
 		}
 	});
